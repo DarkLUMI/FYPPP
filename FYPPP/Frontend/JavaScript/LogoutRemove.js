c019287baa2380065b5952cf96b661f389b0993c
Sirius-13 <cwxdesmond2002@gmail.com>
2024-06-25 16:58:58 +0800

    Remove exixsting folder

--- FYPPP/Frontend/JavaScript/LogoutRemove.js
+++
@@ -1,8 +0,0 @@
-function logout() {
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('USER_EMAIL');
-    sessionStorage.removeItem('USER_NAME');
-    sessionStorage.removeItem('USER_TYPE');
-
-    window.location.href = '../HTML_code/index.html';
-}
